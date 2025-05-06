4e612a48c329e29fdd0e82311a6b6816ab5d6b7a
Christian Alvarado <tianqueal@gmail.com>
2025-05-06 18:16:14 +0200

    feat: add ToastContainer for notifications in Layout component

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router';
 import Header from '../components/Header';
+import { ToastContainer, Bounce } from 'react-toastify';
 
 export default function Layout() {
   return (
@@ -8,6 +9,21 @@ export default function Layout() {
       <main className="mx-10 mb-10 flex flex-col items-center justify-center lg:mx-20 lg:mb-20">
         <Outlet />
       </main>
+
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        limit={3}
+        hideProgressBar={false}
+        newestOnTop={true}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        stacked
+        theme="dark"
+        transition={Bounce}
+      />
     </>
   );
 }
