613302cd4ccd064aa2cc40f3b0b616c71a82ad23
Christian Alvarado <tianqueal@gmail.com>
2025-05-08 10:35:07 +0200

    feat: add environment variable definitions for asset and patch notes URLs

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -6,5 +6,18 @@ export default defineConfig(({ mode }) => {
     esbuild: {
       drop: mode === 'production' ? ['console', 'debugger'] : [],
     },
+    define: {
+      'process.env.ASSET_BASE_URL': JSON.stringify(process.env.ASSET_BASE_URL),
+      'process.env.MANIFEST_URL': JSON.stringify(process.env.MANIFEST_URL),
+      'process.env.PATCH_NOTES_BASE_URL': JSON.stringify(
+        process.env.PATCH_NOTES_BASE_URL,
+      ),
+      'process.env.PATCH_NOTES_URL': JSON.stringify(
+        process.env.PATCH_NOTES_URL,
+      ),
+      'process.env.VITE_PATCH_NOTES_BASE_URL': JSON.stringify(
+        process.env.VITE_PATCH_NOTES_BASE_URL,
+      ),
+    },
   };
 });
